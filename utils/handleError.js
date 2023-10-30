15d18dcbde15ca8781dc5304fba773f17f85805c
Maximiliano Principe <mlapeducacionit@gmail.com>
2023-10-30 19:23:11 -0300

    Borro carpeta data del repo y agrego db.productos.js

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,6 @@
 
 const handleError = (res, mensaje = "Algo sucedió", codigo = 500) => {
-    return res.status(codigo).res.json({ ok: false, error: mensaje})
+    return res.status(codigo).json({ ok: false, error: mensaje})
 }
 
 export default handleError
